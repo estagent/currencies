d5d3667349f9206c37d023843be7674ae4e66160
estagent <estagent@chip.me>
2020-12-28 07:06:26 +0300

    booting updated, package update

--- src/index.js
+++ src/index.js
@@ -1,81 +1,96 @@
-import Preference from '@revgaming/preference';
-import {mergeOptions} from '@revgaming/helpers';
-import translations from './lang';
-import currencies from './currencies';
+import {mergeOptions} from '@revgaming/helpers'
+import {mergeTranslations} from '@revgaming/languages'
+import {config} from '@revgaming/config'
+import {getCountry} from '@revgaming/location'
 
-let options;
+import Preference from '@revgaming/preference'
+import translations from './lang'
+import currencies from './currencies'
 
-const defaults = {
-  iconMainClass: 'icon',
-  iconPrefix: 'ia-',
-};
+let options
 
-const currency_id = (code) => {
-  return code ? currencies[code] : currency_id(currency_code());
-};
+export const bootCurencies = opts => {
+    options = mergeOptions(opts, {
+        iconMainClass: 'icon',
+        iconPrefix: 'ia-',
+    })
 
-const currency_code = (id) => {
-  if (id) {
-    for (let currency_code of Object.keys(currencies)) {
-      if (currencies[currency_code] === id) return currency_code;
+    mergeTranslations('currencies', translations)
+
+    detectCurrencies()
+    return {
+        currency: currency,
+        currency_id: currency_id,
+        currency_code: currency_code,
+        currency_name: currency_name,
+        currency_icon: currency_icon,
+        setCurrencyCode: setCurrencyCode,
     }
-    throw `currency id unknown ${id}`;
-  }
-  return Preference.get('currency', config('app.currency'));
-};
+}
 
-const currency_name = (code) =>
-  __('currencies.codes.'.concat(code ? code : currency_code()));
+export const currency_id = code => {
+    return code ? currencies[code] : currency_id(currency_code())
+}
 
-const currency_icon = (code) => {
-  return options.iconMainClass
-    .concat(' ')
-    .concat(options.iconPrefix)
-    .concat(code ?? currency_code());
-};
-
-const currency = (code) => {
-  if (!code) code = currency_code();
-  return {
-    id: currencies[code],
-    code: code,
-    name: currency_name(code),
-    class: currency_icon(code),
-  };
-};
-
-const setCurrencyCode = (code) => {
-  if (code && Object.keys(currencies).includes(code)) {
-    config({'app.currency': code});
-
-    return true;
-  }
-  return false;
-};
-
-export default function(opts) {
-  options = mergeOptions(opts, defaults);
-
-  if (window['mergeTranslations'])
-    mergeTranslations('currencies', translations);
-
-  return {
-    currencies: (codes) => {
-      const arr = [];
-      for (let code of Object.keys(currencies)) {
-        if (codes) {
-          if (codes.includes(code)) arr.push(currency(code));
-        } else {
-          arr.push(currency(code));
+export const currency_code = id => {
+    if (id) {
+        for (let currency_code of Object.keys(currencies)) {
+            if (currencies[currency_code] === id) return currency_code
         }
-      }
-      return arr;
-    },
-    currency: currency,
-    currency_id: currency_id,
-    currency_code: currency_code,
-    currency_name: currency_name,
-    currency_icon: currency_icon,
-    setCurrencyCode: setCurrencyCode,
-  };
+        throw `currency id unknown ${id}`
+    }
+    return Preference.get('currency', config('app.currency'))
+}
+
+export const currency_name = code =>
+    __('currencies.codes.'.concat(code ? code : currency_code()))
+
+export const currency_icon = code => {
+    return options.iconMainClass
+        .concat(' ')
+        .concat(options.iconPrefix)
+        .concat(code ?? currency_code())
+}
+
+export const currency = code => {
+    if (!code) code = currency_code()
+    return {
+        id: currencies[code],
+        code: code,
+        name: currency_name(code),
+        class: currency_icon(code),
+    }
+}
+
+export const setCurrencyCode = code => {
+    if (code && Object.keys(currencies).includes(code)) {
+        config({'app.currency': code})
+
+        return true
+    }
+    return false
+}
+
+export const getCurrencies = codes => {
+    const arr = []
+    for (let code of Object.keys(currencies)) {
+        if (codes) {
+            if (codes.includes(code)) arr.push(currency(code))
+        } else {
+            arr.push(currency(code))
+        }
+    }
+    return arr
+}
+
+const detectCurrencies = () => {
+    if (setCurrencyCode(Preference.get('currency'))) return true
+    else {
+        const country = getCountry()
+        return setCurrencyCode(
+            country
+                ? country.curr
+                : config('app.fallback_currency', config('app.currency')),
+        )
+    }
 }
